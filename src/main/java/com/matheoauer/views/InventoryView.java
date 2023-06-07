1d24100e9956df68757399f94b4603b7387dc226
amatheo <59826260+amatheo@users.noreply.github.com>
2023-06-07 11:35:05 +0200

    Fix Inv view

--- src/main/java/com/matheoauer/views/InventoryView.java
+++ src/main/java/com/matheoauer/views/InventoryView.java
@@ -20,12 +20,12 @@ import java.util.logging.Logger;
 public class InventoryView extends JPanel implements Observer {
 
     private final HashMap<String, JLabel> inventory;
-    private JLabel labelMoney = new JLabel("Argent : 0");
+    private final JLabel labelMoney = new JLabel("Argent : 0");
 
     public InventoryView(HashMap<String, Integer> inventory) {
-        setBorder(BorderFactory.createEmptyBorder(0,0,0,5));
+        setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
         this.inventory = new HashMap<>();
-        this.setLayout(new GridLayout(inventory.size() + 1,1));
+        this.setLayout(new GridLayout(inventory.size() + 1, 1));
 
         for (String vegetableName : inventory.keySet()) {
             JLabel label = new JLabel("x" + inventory.get(vegetableName));
@@ -36,6 +36,7 @@ public class InventoryView extends JPanel implements Observer {
             add(label);
         }
         add(labelMoney);
+        this.setPreferredSize(new Dimension(140, 0));
     }
 
     @Override
@@ -45,9 +46,9 @@ public class InventoryView extends JPanel implements Observer {
             return;
         }
 
-        for (String vegetableName : inventory.getInventory().keySet()){
+        for (String vegetableName : inventory.getInventory().keySet()) {
             JLabel label = this.inventory.get(vegetableName);
-            if (label != null){
+            if (label != null) {
                 label.setText("x" + inventory.getInventory().get(vegetableName));
                 try {
                     URL resource = getClass().getClassLoader().getResource("claimed_vegetable_sound.wav");
